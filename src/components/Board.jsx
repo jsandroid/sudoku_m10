7773b600444be2e09a659ccd5daa0ee033c3e968
Jonathan Sanchez <jsandroid@gmail.com>
2022-02-21 15:04:51 -0500

    fix game state update

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,17 +1,17 @@
-import { Button, Grid, styled, TextField } from "@mui/material";
-import React, { useEffect, useState } from "react";
+import {Grid} from "@mui/material";
+import React from "react";
 import SudokuCell from "./SudokuCell";
-
+import { v4 as uuidv4 } from 'uuid';
 
 export default function Board({ game, action }) {
-  
+
   return (
     <div id='gameboard'>
       <Grid direction='column' alignContent='center' container>
         {game?.gameboard.map((row, rowIndex) => (
-          <Grid direction='row' container > 
-            {row.col.map((cell, colIndex) => (
-              <Grid item >
+          <Grid direction='row' container key={()=> uuidv4()}> 
+            {row?.col?.map((cell, colIndex) => (
+              <Grid item key={()=> uuidv4()}>
                 <SudokuCell cell={cell} update={action}/>
               </Grid>
             ))}
--- src/components/SudokuCell.jsx
+++ src/components/SudokuCell.jsx
@@ -1,28 +1,33 @@
-import React, { useState } from "react";
+import React from "react";
 import { styled, TextField } from "@mui/material";
 
 const Cell = styled(TextField)(({ theme }) => ({
   width: '55px',
-  height: '55px',
+  height: '55px', 
+  '& .MuiOutlinedInput' : {
+
+  } 
 }),)
 
 
 
-export default function SudokuCell({cell, action }) {
+export default function SudokuCell({ cell, update }) {
   
   function handleChange(e) {
     e.preventDefault();
     const value = "" ? null : parseInt(e.target.value);
-    action({...cell, value}, e)
+    update({...cell, value}, e)
   }
 
   return(
     <Cell 
       onChange={handleChange} 
-      inputProps={{style: { textAlign: 'center'}}} 
-      value={(cell.value > 0) ? cell.value : null}
-      disabled={cell.readOnly}>
-
+      inputProps={{style: { textAlign: 'center', borderRadius: 0}}} 
+      value={(cell.value > 0) ? cell.value : ""}
+      disabled={cell.readOnly}
+      error={(cell.value > 9 && cell.value > 0) ? true : false}
+      jmsr={cell.answered}
+      >
     </Cell>
 
   )
